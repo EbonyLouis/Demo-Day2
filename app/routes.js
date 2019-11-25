0b497096f3442184e8430a007ae9ffd54d6e85c1
Ebony <ebonyjlouis@gmail.com>
2019-11-24 19:42:39 -0500

    edits to search

---
+++ app/routes.js
@@ -0,0 +1,251 @@
+module.exports = function(app, passport, multer, db) {
+
+  // normal routes =================================================
+
+
+
+  // show the home page (will also have our login links)
+  app.get('/', function(req, res) {
+    res.render('login.ejs', {message: ""});
+  });
+
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', {message: ""});
+  });
+  app.get('/patientSearch', function(req, res) {
+    res.render('search.ejs', {patientList: null});
+  });
+  // app.get('/test', function(req, res) {
+  //     res.render('search.ejs', {message: ""});
+  // });
+
+  // patient info grabbing the info from the database to render on the page
+  app.post('/patientSearch', function(req, res) {
+    db.collection('patients').find({keyword: req.body.keyword}).toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('search.ejs', {
+        // user : req.user,
+        patientList: result
+      })
+    })
+  });
+
+  // single patient page information below to find the page
+
+  app.get('/patient', isLoggedIn, function(req, res) {
+    console.log('patient keyword', req.query.keyword)
+    db.collection('patients').findOne({name:req.query.name},(err, result) => {
+      console.log('patient:', result)
+      if (err) return console.log(err)
+      res.render('pprofile.ejs', {
+        patient: result
+      })
+    })
+  });
+
+// Images saving them to the database
+// need help getting the url to match the keyword when updating images
+// var upload = multer({ dest: "./public/images/uploads/"});
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + ".png")
+    }
+});
+var upload = multer({storage: storage});
+
+app.post('/uploadImage', upload.single('patientPhotos'), (req, res, next) => {
+  // console.log(req)
+    insertDocuments(db, req, 'images/uploads/' + req.file.originalname, () => {
+      res.redirect(req.get('referer'));
+        // res.redirect('/patient?name=<%=patientList[i].name')
+    });
+});
+
+var insertDocuments = function(db, req, filePath, callback) {
+    var collection = db.collection('patients');
+      console.log('what im looking for:' , req.body.name)
+      console.log('Here:',filePath)
+    collection.findOneAndUpdate({name: req.body.name}, {
+
+      $set: {
+        image: filePath
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: false
+    }, (err, result) => {
+      if (err) return res.send(err)
+      callback(result)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post('/uploadImage' , upload.single('patientPhotos'), (req, res) =>{
+//   console.log('hi');
+//   console.log('this is the image:' ,req.file);
+//   db.collection('patients').aggregate([
+//     {
+//        $set:{
+//          "image" :req.file.filename,
+//        }
+//     },{
+//       sort: {_id: -1},
+//       upsert: true
+//     }, (err, result) => {
+//       if (err) return res.send(err)
+//       res.send(result)
+//     }
+//   ])
+// })
+
+
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function(req, res) {
+    db.collection('messages').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('profile.ejs', {
+        user : req.user
+      })
+    })
+  });
+
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  //
+
+  // board routes ===============================================================
+
+  app.post('/messages', (req, res) => {
+    db.collection('messages').save({name: req.body.name, msg: req.body.msg,
+      thumbUp: 0, thumbDown:0}, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/profile')
+      })
+    })
+
+    app.put('/messages', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbUp:req.body.thumbUp + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.put('/messages2', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbUp:req.body.thumbUp - 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.delete('/messages', (req, res) => {
+      db.collection('messages').findOneAndDelete({name: req.body.name,
+        msg: req.body.msg}, (err, result) => {
+          if (err) return res.send(500, err)
+          res.send('Message deleted!')
+        })
+      })
+
+      // =============================================================================
+      // AUTHENTICATE (FIRST LOGIN) ==================================================
+      // =============================================================================
+
+      // locally --------------------------------
+      // LOGIN ===============================
+      // show the login form
+      app.get('/login', function(req, res) {
+        const loginMessage = req.flash('loginMessage');
+        res.render('login.ejs', { message: loginMessage });
+      });
+
+      // process the login form
+      app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/patientSearch', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+      }));
+
+      // SIGNUP =================================
+      // show the signup form
+      app.get('/signup', function(req, res) {
+
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+      });
+
+      // process the signup form
+      app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+      }));
+
+      // =============================================================================
+      // UNLINK ACCOUNTS =============================================================
+      // =============================================================================
+      // used to unlink accounts. for social accounts, just remove the token
+      // for local account, remove email and password
+      // user account will stay active in case they want to reconnect in the future
+
+      // local -----------------------------------
+      app.get('/unlink/local', isLoggedIn, function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+          res.redirect('/profile');
+        });
+      });
+
+    };
+
+    // route middleware to ensure user is logged in
+    function isLoggedIn(req, res, next) {
+      if (req.isAuthenticated())
+      return next();
+
+      res.redirect('/');
+    }
